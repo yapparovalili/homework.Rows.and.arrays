18b5c0db6a8e21070dc2e53c835c181132d921ce
Liliya Yapparova <yapparovalili@yandex.ru>
2022-11-20 21:35:43 +0700

    Changed

--- src/test/java/ru/netology/stats/homework/Rows/and/arrays/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/homework/Rows/and/arrays/StatsServiceTest.java
@@ -66,4 +66,16 @@ public class StatsServiceTest {
 
     }
 
+    @Test
+    public void highAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.lowAverage(sales);
+        long expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
 }
